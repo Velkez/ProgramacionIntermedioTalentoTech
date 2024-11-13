c8bb72fecf123f4bcb7b2ee387f4fddfbbd8e5dd
Elkin Vasquez <velkezdev@gmail.com>
2024-11-13 10:26:20 -0500

    practicando excepciones (corrección ultimo ejercicio)

--- SeccionPython/excepciones.py
+++ SeccionPython/excepciones.py
@@ -58,6 +58,7 @@ def main():
 # DEBE MOSTRAR UN MENSAJE DE QUE MATERIA VA PERDIENDO, SI POR ERROR EL USUARIO INGRESA UNA NOTA MAYOR DE 
 # 5 O NEGATIVA, EVITAR EL ERROR POSIBLE, Y REALICE LA SUBSANACION
 import re
+import copy
 
 # Función para saber si la nota esta dentro del rango de 0 a 5. En caso de no estarlo, lanzará una excepción de error.
 def rango_nota(nota):
@@ -67,16 +68,17 @@ def rango_nota(nota):
         return nota
     
 # Función para agregar las notas del estudiante para la materia digitada. Este muestra formateado las notas digitadas y el promedio de la materia.
-def generar_notas(estudiante, materia):
-    notas = {}
+def generar_notas(estudiante, materia,):
     try: 
         print (f"\nDigite las notas del estudiante {estudiante} para la materia de {materia}.")
         nota_1 = rango_nota(float(input("Nota 1 (20%): ")))
         nota_2 = rango_nota(float(input("Nota 2 (20%): ")))
         nota_3 = rango_nota(float(input("Nota 3 (20%): ")))
         parcial = rango_nota(float(input("Parcial (40%): ")))
+        
         promedio = ((((nota_1 + nota_2 + nota_3) / 3) * 0.6) + (parcial * 0.4)) / 1
-        notas[materia] = round(promedio, 1)
+        # notas[materia] = round(promedio, 1)
+        
         print (f"\nSe agregaron con exito las notas de {materia} del estudiante {estudiante}.")
         print (f"""Las notas son las siguientes: 
                Nota 1 (20%) |  Nota 2 (20%) |  Nota 3 (20%) | Parcial (40%)
@@ -87,54 +89,66 @@ def generar_notas(estudiante, materia):
         """)
     except ValueError as e:
         print (e)
-    return notas
-
-# Esta función solo se encarga de organizar las notas en una biblioteca según el estudiante digitado.
-def organizar_notas(estudiante, nota):
-    notas = {}
-    notas[estudiante] = nota
-    return notas
+    
+    return round(promedio, 1)
         
 # Fución para agregar un estudiante dentro de una lista de estudiantes. Este crea la lista y almacena el nombre del estudiante dentro de esta.
 def agregar_estudiantes():
     expresion = r"^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$"
     estudiante = input("\nNombre del estudiante a agregar: ")
+    
     if re.match(expresion, estudiante):
         print (f"\nEl estudiante {estudiante} fue agregado con exito.")
     else:
         raise ValueError("Nombre no valido. Este no debe contener signos ni numeros.")
+    
     return estudiante
 
+# Función para asignar las calificaciones a cada estudiante.
+def calificar_estudiante(materias, estudiante, notas={}):
+    nota = 0.0
+    nota_base = {"matematicas" : nota, "fisica": nota, "quimica": nota}
+    expresion = r"^[sSnN]?$"
+    
+    while True:
+        print ("\nTenemos el siguiente listado de materias: ")
+        print ("\n".join(f"-. {nombre}" for i, nombre in enumerate(materias)))
+        
+        materia = input ("\n¿Qué materia desea calificar?: ")
+        materia = materia.lower()
+        
+        if estudiante not in notas:
+            notas[estudiante] = copy.deepcopy(nota_base)
+        
+        nota = generar_notas(estudiante, materia)
+        
+        if materia in notas[estudiante]:
+            notas[estudiante][materia] = nota
+        else:
+            raise ValueError(f"\n'{materia}' no se encuentra registrado. Por favor, digite alguna materia del listado.")
+        
+        print (f"\nLas notas van de la siguiente manera: \n{notas}")
+        
+        pregunta = input ("\n¿Desea calificar otra materia? [S/n]:")
+        if re.match(expresion, pregunta) and re.match(r"^[nN]+$", pregunta):
+            break
+        
+    return notas
 
 def main():
     expresion = r"^[sSnN]?$"
     materias = ["matematicas", "fisica", "quimica"]
-    notas = {}
     while True:
         try:
             respuesta = input ("\n¿Desea agregar un nuevo estudiante? [S/n]: ")
             if re.match(expresion, respuesta):
                 if respuesta == "s" or respuesta == "S" or respuesta == "":
                     estudiante = agregar_estudiantes()
-                    while True:
-                        print ("\nTenemos el siguiente listado de materias: ")
-                        print ("\n".join(f"{i+1}. {nombre}" for i, nombre in enumerate(materias)))
-                        materia = input ("\n¿Qué materia desea calificar?: ")
-                        materia = materia.lower()
-                        if materia in materias:
-                            nota = generar_notas(estudiante, materia)
-                            notas[estudiante] = nota
-                            print (f"\nLas notas van de la siguiente manera: \n{notas}")
-                        else:
-                            print (f"\n'{materia}' no se encuentra registrado. Por favor, digite alguna materia del listado.")
-                        
-                        pregunta = input ("\n¿Desea calificar otra materia? [S/n]:")
-                        if re.match(expresion, pregunta) and re.match(r"^[nN]+$", pregunta):
-                            break
+                    calificar_estudiante(materias, estudiante)
                 else:
                     break
             else:
-                print (f"\n'{respuesta}' no es una respuesta valida.")
+                raise ValueError(f"\n'{respuesta}' no es una respuesta valida.")
         except ValueError as e:
             print (e)
 
